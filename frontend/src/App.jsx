b19fa795a654ac9a97a540a1036dd7d87732fb0a
Archontas <173107956+Archontas123@users.noreply.github.com>
2026-03-28 11:29:01 -0400

    feat: add app shell with React Router, MockDataProvider, and global styles

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { MockDataProvider } from './context/MockDataContext'
+import TopNav from './components/TopNav/TopNav'
+import Home from './features/home/Home'
+import StubPage from './pages/StubPage'
+import styles from './App.module.css'
+
+export default function App() {
+  return (
+    <MockDataProvider>
+      <BrowserRouter>
+        <TopNav />
+        <main className={styles.main}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/network" element={<StubPage title="My Network" />} />
+            <Route path="/jobs" element={<StubPage title="J*bs" />} />
+            <Route path="/messaging" element={<StubPage title="Messaging" />} />
+            <Route path="/notifications" element={<StubPage title="Notifications" />} />
+            <Route path="/me" element={<StubPage title="Me" />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </MockDataProvider>
+  )
+}
